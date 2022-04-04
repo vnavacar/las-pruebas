177a41195bc76d83f290dab25cb9b6e5b752ead9
vnavacar <vnavacar@myuax.com>
2022-04-04 17:14:21 +0200

    Update ClaseCadena.java

--- la prueba/src/ClaseCadena.java
+++ la prueba/src/ClaseCadena.java
@@ -8,6 +8,8 @@ class ClaseCadena{
 
         if (nombre.length() == 0)
             return "";
+        if (nombre.contains("0")||nombre.contains("1")||nombre.contains("2")||nombre.contains("3")||nombre.contains("4")||nombre.contains("5")||nombre.contains("6")||nombre.contains("7")||nombre.contains("8")||nombre.contains("9"))
+            return "";
 
             System.out.print(Character.toUpperCase(nombre.charAt(0)));
             Iniciales= Iniciales + nombre.charAt(0);
