c2c34cbd9a540b4ac26e2a4f76eefcb496d6ff7b
vnavacar <vnavacar@myuax.com>
2022-04-04 17:25:41 +0200

    sad

--- la prueba/src/App.java
+++ la prueba/src/App.java
@@ -4,7 +4,10 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         String iniciales = ClaseCadena.DevuelveIniciales("Andreas Dulac"); 
-        System.out.println("El metodo devuelve: "+iniciales);
+        System.out.println("Intentando: Andreas Dulac,El metodo devuelve: "+iniciales);
+
+        System.out.println("Intentando: '''' El metodo devuelve: "+ClaseCadena.DevuelveIniciales(""));
+        System.out.println("Intentando: 1234 4231 El metodo devuelve: "+ClaseCadena.DevuelveIniciales("1234 4231"));
         Scanner teclado = new Scanner(System.in);
         String entrada;
 
@@ -12,7 +15,7 @@ public class App {
             System.out.println("Introduzca otro nombre");
             entrada = teclado.nextLine();
             System.out.println("El metodo devuelve: "+ClaseCadena.DevuelveIniciales(entrada) );
-        }while(entrada != "no");
+        }while(entrada.equals("no") == false);
         teclado.close();
 
     }
--- la prueba/src/ClaseCadena.java
+++ la prueba/src/ClaseCadena.java
@@ -11,13 +11,13 @@ class ClaseCadena{
         if (nombre.contains("0")||nombre.contains("1")||nombre.contains("2")||nombre.contains("3")||nombre.contains("4")||nombre.contains("5")||nombre.contains("6")||nombre.contains("7")||nombre.contains("8")||nombre.contains("9"))
             return "";
 
-            System.out.print(Character.toUpperCase(nombre.charAt(0)));
-            Iniciales= Iniciales + nombre.charAt(0);
+            //System.out.print(Character.toUpperCase(nombre.charAt(0)));
+            Iniciales= Iniciales + nombre.charAt(0)+".";
 
             for (int i = 1; i < nombre.length() - 1; i++){
                 if (nombre.charAt(i) == ' '){
-                System.out.print(" " + Character.toUpperCase(nombre.charAt(i + 1)));
-                Iniciales = Iniciales + nombre.charAt(i+1);
+                //System.out.print(" " + Character.toUpperCase(nombre.charAt(i + 1)));
+                Iniciales = Iniciales + nombre.charAt(i+1)+".";
             }
         }
         System.out.println("");
